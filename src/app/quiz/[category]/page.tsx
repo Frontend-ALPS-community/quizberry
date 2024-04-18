dd39df30e711d09fba00fc3b209cfb1b9051e5fa
nasilKiM <nasikun@naver.com>
2024-04-18 23:38:39 +0900

    :memo: MockData : data 리스트 생성

--- src/app/quiz/[category]/page.tsx
+++ src/app/quiz/[category]/page.tsx
@@ -1,5 +1,57 @@
-const quizCategory = () => {
-  return <div>category</div>
+import React, { useState, useEffect } from 'react'
+
+interface Question {
+  id: number
+  category_id: number
+  number: number
+  question: string
 }
 
-export default quizCategory
+interface Category {
+  id: number
+  name: string
+}
+
+const QuizList: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([])
+  const [questions, setQuestions] = useState<Question[]>([])
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('/quizData.json')
+  //       const data = await response.json()
+  //       setCategories(data.categories)
+  //       setQuestions(data.questions)
+  //     } catch (error) {
+  //       console.error('Error', error)
+  //     }
+  //   }
+
+  //   fetchData()
+  // }, [])
+
+  return (
+    <div>
+      <div>퀴즈카테고리</div>
+      <div>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <a href={`/category/${category.id}`}>{category.name}</a>
+          </li>
+        ))}
+      </div>
+
+      <h2>문제</h2>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            <a href={`/question/${question.id}`}>Question {question.number}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default QuizList
