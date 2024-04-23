a348771746676f3dada009277ad17d9672a21ebb
nasilKiM <nasikun@naver.com>
2024-04-24 00:41:55 +0900

    :art: style: 스타일코드일부수정

--- src/app/quiz/[category]/[number]/page.tsx
+++ src/app/quiz/[category]/[number]/page.tsx
@@ -1,5 +1,12 @@
+// 문제타입이 뭐냐에 따른 컴포넌트 2-3개 따로 만들어서 구별해서 보여주기
+
 const quizDetailPage = () => {
-  return <div>detail 문제 페이지</div>
+  return (
+    <>
+      <div>detail 문제 페이지</div>
+      <div className="w-full h-[400px] border bg-gray-100"></div>
+    </>
+  )
 }
 
 export default quizDetailPage
